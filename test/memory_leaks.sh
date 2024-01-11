28f0daa5971a59cd0f41614b289d9281889a6d47
Bernhard Gstrein <bernhard-gstrein@mailbox.org>
2024-01-11 16:23:34 +0100

    Fix testing when no valgrind

--- test/memory_leaks.sh
+++ test/memory_leaks.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if valgrind is installed
+if ! command -v valgrind &> /dev/null
+then
+    echo "valgrind not installed, exiting with code 0."
+    exit 0
+fi
+
 # Run valgrind and capture its output
 output=$(valgrind ./unit_test 2>&1)
 
